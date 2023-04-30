5da5d27bd151405c8ce9585c3031ecc2edac19e4
Prince Felix <princeafelix@gmail.com>
2023-04-29 21:53:12 -0400

    Skill section responsive

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,29 +5,27 @@ import profile from '../../assets/PRIMG.jpg';
 
 export const About = () => {
   return (
-    <div>
-      <section className={classes.aboutsection} id="about">
-        <div className={sharedstyle["container"]}>
-          <h1 className={sharedstyle["section-title"]}>About</h1>
-          <span className={sharedstyle["divider"]}></span>
-        </div>
-        <div className={classes.aboutcontent}>
-          <div className={classes.myInfo}>
-            <div id="profile" className={classes['profile']}>
-              <img className={classes.image} src={profile} alt="myPic" />
-            </div>
-          </div>
-          <div className={classes["about-text"]}>
-            <p className={sharedstyle.p}>I'm Prince!<br /> A recent Computer Engineering Technology - Computing Science Graduate with frontend and backend skills and with a strong interest in software development. My passion for technology started early when I looked up to my cousin who graduated with Bachelor of Information Technology. Since then, I found myself enjoying the software side of it.</p>
-            <p className={sharedstyle.p}>My main focus today is to land a job that will help me excel in learning different things in this field and will help me explore the things I could do that I thought I couldn't.</p>
-            <p className={sharedstyle.p}>Well-organized developer, love challenges, problem solver with great passion for programming.</p>
-            <p className={sharedstyle.p}><a href="#contact">Say hello.</a></p>
+    <section className={classes.aboutsection} id="about">
+      <div className={sharedstyle["container"]}>
+        <h1 className={sharedstyle["section-title"]}>About</h1>
+        <span className={sharedstyle["divider"]}></span>
+      </div>
+      <div className={classes.aboutcontent}>
+        <div className={classes.myInfo}>
+          <div id="profile" className={classes['profile']}>
+            <img className={classes.image} src={profile} alt="myPic" />
           </div>
         </div>
-        <div className={sharedstyle['end-section']} />
+        <div className={classes["about-text"]}>
+          <p className={sharedstyle.p}>I'm Prince!<br /> A recent Computer Engineering Technology - Computing Science Graduate with frontend and backend skills and with a strong interest in software development. My passion for technology started early when I looked up to my cousin who graduated with Bachelor of Information Technology. Since then, I found myself enjoying the software side of it.</p>
+          <p className={sharedstyle.p}>My main focus today is to land a job that will help me excel in learning different things in this field and will help me explore the things I could do that I thought I couldn't.</p>
+          <p className={sharedstyle.p}>Well-organized developer, love challenges, problem solver with great passion for programming.</p>
+          <p className={sharedstyle.p}><a href="#contact">Say hello.</a></p>
+        </div>
+      </div>
+      <div className={sharedstyle['end-section']} />
 
-      </section>
-    </div>
+    </section>
   )
 }
 
