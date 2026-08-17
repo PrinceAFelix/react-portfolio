f1ff1118172ad4919789f5982e27a24cc06a0db4
PrinceAFelix <princefelix23@icloud.com>
2026-08-17 15:00:29 -0400

    Update new projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
+import ContinuefyProject from './pages/ContinuefyProject'
 
 const PageNotFound = lazy(() => import("./pages/PageNotFound"))
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/continuefy" element={<ContinuefyProject />} />
           <Route path="*" element={PageNotFound} />
         </Routes>
       </Router>
